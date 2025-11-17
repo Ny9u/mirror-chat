47680def603f36ed94e8d5cdb4922a8e7444b411
ny9u <604098901@qq.com>
2025-11-17 19:26:27 +0800

    删除无用接口

--- src/services/user.js
+++ src/services/user.js
@@ -1,21 +1,6 @@
 import { Request } from "@/utils/request";
 import { api } from "@/config/api";
 
-export const getAvatar = async (params) => {
-  const res = await Request(api.avatar, "GET", params);
-  return res;
-};
-
-export const getName = async (params) => {
-  const res = await Request(api.name, "GET", params);
-  return res;
-};
-
-export const getUserInfo = async (params) => {
-  const res = await Request(api.getUserInfo, "GET", params);
-  return res;
-};
-
 export const login = async (userData) => {
   const res = await Request(api.login, "POST", userData);
   return res;
